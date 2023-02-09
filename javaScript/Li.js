33227051534de42ca39bde7951dd450cb197a66b
Javier Carrasco <javierdeveloper08@gmail.com>
2023-02-09 17:55:13 -0400

    Reproduciendo Audio

--- javaScript/Li.js
+++ javaScript/Li.js
@@ -8,6 +8,9 @@ export function liMusicList() {
         <img src="${e.src}" alt="${e.name}" class="music-list__img" />
         <figcaption class="music-list__name">${e.name}</figcaption>
         </figure>`;
+    $li.dataset.audio = e.audio;
+    $li.dataset.src = e.src;
+    $li.dataset.name = e.name;
     $li.classList.add("music-list__li");
     li.push($li);
   });
--- javaScript/index.js
+++ javaScript/index.js
@@ -1,4 +1,36 @@
+import { dataMusic } from "./data.js";
 import { liMusicList } from "./Li.js";
-
-const $musicListUl = document.querySelector(".music-list__ul");
+let boolean = false;
+const $AUDIO = document.getElementById("audio");
+const $musicListUl = document.querySelector(".music-list__ul"),
+  $controls = document.querySelector(".container-reproductor__controls");
 $musicListUl.append(...liMusicList());
+
+$musicListUl.addEventListener("click", (e) => {
+  const LI = e.target.closest(".music-list__li");
+  if (LI) {
+    boolean = true;
+    const $playPause = document.querySelector(
+      ".container-reproductor__i-modificare"
+    );
+    $playPause.classList.replace("fa-play", "fa-pause");
+    const $FIGURE = document.querySelector(".container-reproductor__figure");
+    $FIGURE.firstElementChild.src = LI.dataset.src;
+    $FIGURE.lastElementChild.textContent = LI.dataset.name;
+    $AUDIO.src = LI.dataset.audio;
+    $AUDIO.play();
+  }
+});
+$controls.addEventListener("click", (e) => {
+  if (boolean) {
+    if (e.target.matches(".container-reproductor__i-modificare")) {
+      if (e.target.classList.contains("fa-pause")) {
+        e.target.classList.replace("fa-pause", "fa-play");
+        $AUDIO.pause();
+      } else {
+        e.target.classList.replace("fa-play", "fa-pause");
+        $AUDIO.play();
+      }
+    }
+  }
+});
