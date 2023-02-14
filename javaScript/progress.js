5b1ed3a80f72578eda417e819b3fdc2c4f370e25
Javier Carrasco <javierdeveloper08@gmail.com>
2023-02-13 21:08:08 -0400

    Progressive

--- javaScript/progress.js
+++ javaScript/progress.js
@@ -2,7 +2,6 @@ import { $AUDIO, $CIRCLE, $CONTENT_P, $PLAY, $TEMPO } from "./constantes.js";
 import { next } from "./next_prev.js";
 
 const $PARENT = $CIRCLE.parentElement;
-const $GRANDFATHER = $PARENT.parentElement;
 export function progressive() {
   let { offsetHeight: x, offsetWidth: y } = $PARENT;
   $CIRCLE.style.top = `${x / 2 - $CIRCLE.clientHeight / 2}px`;
@@ -18,22 +17,15 @@ export function run() {
       ),
       secondsStatic = Math.floor((miliSecondStatic % (1000 * 60)) / 1000);
     $TEMPO.lastElementChild.textContent = `${minuteStatic}:${secondsStatic}`;
-    $AUDIO.currentTime = $AUDIO.duration;
   }, 1000);
 
   let setRun = setInterval(() => {
-    let minuteStatic = Math.floor(
-      (($AUDIO.duration * 1000) % (1000 * 60 * 60)) / (1000 * 60)
-    );
+    $PARENT.style.width = `${Math.floor(
+      ($AUDIO.currentTime * 100) / $AUDIO.duration
+    )}%`;
     let miliSecondsRun = $AUDIO.currentTime * 1000,
       minuteRun = Math.floor((miliSecondsRun % (1000 * 60 * 60)) / (1000 * 60)),
       secondsRun = "0" + Math.floor((miliSecondsRun % (1000 * 60)) / 1000);
-    // console.log(Math.floor(($AUDIO.currentTime * 10) / 10));
-    // console.log(
-    //   Math.floor(
-    //     ($AUDIO.currentTime * 1000) / ($GRANDFATHER.clientWidth * minuteStatic)
-    //   )
-    // );
     $TEMPO.firstElementChild.textContent = `${minuteRun}:${secondsRun.slice(
       -2
     )}`;
